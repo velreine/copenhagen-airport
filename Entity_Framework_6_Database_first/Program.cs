285507c73c56b23f1560576cd238514ebaf6437e
Nicky Gottlieb Hansen <nicky-g-hansen@hotmail.com>
2020-08-05 09:34:04 +0200

    Initial commit.

---
+++ Entity_Framework_6_Database_first/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Entity_Framework_6_Database_first
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
